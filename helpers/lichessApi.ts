8910dcd4d95e35779921ba418e8f80633a66a183
Victor Ivanovsky <vityukoff@gmail.com>
2019-08-11 22:29:36 +0700

    finished lichessapi and create lichessbot

--- helpers/lichessApi.ts
+++ helpers/lichessApi.ts
@@ -1,7 +1,8 @@
+import * as oboe from 'oboe';
 import * as http from 'http';
 import * as https from 'https';
 import * as querystring from 'querystring';
-import {RoomType} from '../types';
+import {LichessGameEvent, LichessLobbyEvent, RoomType} from '../types';
 
 export default class LichessApi {
     baseUrl: string;
@@ -11,7 +12,10 @@ export default class LichessApi {
     constructor(token: string) {
         this.token = token;
         this.baseUrl = 'lichess.org';
-        this.headers = { 'Authorization': `Bearer ${this.token}` };
+        this.headers = {
+            'Authorization': `Bearer ${this.token}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
     }
 
     get(path: string): Promise<http.IncomingMessage> {
@@ -23,6 +27,7 @@ export default class LichessApi {
     }
 
     acceptChallenge(challengeId: string) {
+        console.log('accept', `/api/challenge/${challengeId}/accept`);
         return this.post(`/api/challenge/${challengeId}/accept`);
     }
 
@@ -39,30 +44,44 @@ export default class LichessApi {
     }
 
     abortGame(gameId: string) {
-        return this.post(`api/bot/game/${gameId}/abort`);
+        return this.post(`/api/bot/game/${gameId}/abort`);
     }
 
     resignGame(gameId: string) {
-        return this.post(`api/bot/game/${gameId}/resign`);
+        return this.post(`/api/bot/game/${gameId}/resign`);
+    }
+
+    streamLobby(cb: (event: LichessLobbyEvent) => void) {
+        return this.createStream<LichessLobbyEvent>('/api/stream/event', cb);
+    }
+
+    streamGame(gameId: string, cb: (event: LichessGameEvent) => void) {
+        return this.createStream<LichessGameEvent>(`/api/bot/game/stream/${gameId}`, cb);
     }
 
     createStream<T>(path: string, cb: (event: T) => void) {
-
+        oboe({
+            method: 'GET',
+            url: 'https://' + this.baseUrl + path,
+            headers: this.headers
+        })
+        .node('!', (event) => {
+            cb(event);
+        })
+        .fail((error) => console.error(`STREAM ERROR: ${JSON.stringify(error)}`));
     }
 
     _sendRequest(path: string, method: 'get' | 'post', body?: any): Promise<http.IncomingMessage> {
         return new Promise((resolve, reject) => {
             const request = https.request({
+                protocol: 'https:',
                 host: this.baseUrl,
                 headers: this.headers,
                 path,
                 method
             }, (res) => resolve(res));
 
-            if (body) {
-                console.log(body);
-                request.write(querystring.stringify(body));
-            }
+            request.write(querystring.stringify(body || {}));
             request.on('error', reject);
             request.end();
         })
--- index.ts
+++ index.ts
@@ -1,61 +1,9 @@
 import LichessApi from './helpers/lichessApi';
 import ChessGame from './helpers/chess';
+import LichessBot from './LichessBot';
 
-import {LichessGameEvent, LichessLobbyEvent, RoomType} from './types';
-
-const id = 'olegtrick';
-const token = '3LBpuFgvWMF74HgB';
-let gameId = '';
-const lichessApi = new LichessApi(token);
-
-const createStream = async <T>(path: string, cb: (event: T) => void) => {
-    const response = await lichessApi.get(path);
-
-    response.on('data', (data) => {
-        const streamData = data.toString('utf8').split('\n');
-
-        for (let i = 0; i < streamData.length; i++) {
-            if (!streamData[i]) {
-                continue;
-            }
-
-            const streamEvent = JSON.parse(streamData[i]);
-
-            cb(streamEvent);
-        }
-    });
-};
-
-const challengeEventListener = (event: LichessLobbyEvent) => {
-    if (event.type === 'challenge') {
-        console.log('streamEvent', event);
-        if (!event.challenge.rated && event.challenge.challenger.id === 'victorinthesky') {
-            lichessApi.acceptChallenge(event.challenge.id);
-        } else {
-            lichessApi.declineChallenge(event.challenge.id);
-        }
-    } else {
-        console.log('gameStart', event);
-        lichessApi.sendMessage(event.game.id, RoomType.PLAYER, 'Hello! I am Oleg!');
-        createStream<LichessGameEvent>(`/api/bot/game/stream/${event.game.id}`, gameEventListener);
-    }
-};
-
-const gameEventListener = (event: LichessGameEvent) => {
-    if (event.type === 'gameFull') {
-        gameId = event.id;
-        if (event.white.id === id) {
-            console.log('I am white and move!');
-            lichessApi.makeMove(gameId, 'e2e4');
-        }
-    } else if (event.type === 'gameState'){
-        console.log('I am black and move!');
-        console.log('gameId', gameId);
-        lichessApi.makeMove(gameId, 'e7e5');
-    }
-};
-
-createStream<LichessLobbyEvent>('/api/stream/event', challengeEventListener);
+const botOleg = new LichessBot();
+botOleg.start();
 
 const game1 = new ChessGame();
 const game2 = new ChessGame('r4rk1/pp3qpp/2p1b3/3np1N1/1P2B3/P5PP/3Q1P2/3R1RK1 b - - 0 24');
