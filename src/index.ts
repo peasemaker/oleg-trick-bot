d26d0cb4545309e8ee6fe0784327f53bfe8faea6
Victor Ivanovsky <vityukoff@gmail.com>
2019-10-21 16:31:59 +0700

    move constants

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import ChessGame from './chess/ChessGame';
-import {Piece} from './constants';
+import {sq120} from './constants';
 import LichessBot from './lichess/LichessBot';
 import MinimaxBot from './bots/MinimaxBot';
 
-const game = new ChessGame('6b1/5P2/8/8/3n1k2/8/8/4K2R w K - 0 1');
-
 const botOleg = new LichessBot(MinimaxBot);
 botOleg.start();
+//
+// const game = new ChessGame('6b1/5P2/8/8/3n1k2/8/8/4K2R w K - 0 1');
+
+
+
+
